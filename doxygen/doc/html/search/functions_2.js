7dd7e6d10aee3f714af9896feebcbb76e950f841
Giulio Romualdi <giulio.romualdi@gmail.com>
2018-02-03 15:53:16 +0100

    provided doxygen documentation

---
+++ doxygen/doc/html/search/functions_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['eigensparsematrixtotriplets',['eigenSparseMatrixToTriplets',['../namespaceOsqpEigen_1_1SparseMatrixHelper.html#ad0dac7c63fc97d0ceddadd5145d9b5c6',1,'OsqpEigen::SparseMatrixHelper']]]
+];
---
+++ doxygen/doc/html/search/functions_8.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['updatebounds',['updateBounds',['../classOsqpEigen_1_1Solver.html#a1af39f37500a6bad6896e8e6699880fa',1,'OsqpEigen::Solver']]],
+  ['updategradient',['updateGradient',['../classOsqpEigen_1_1Solver.html#a7d1d9fdd8db3201a0ca2dcafbc289eea',1,'OsqpEigen::Solver']]],
+  ['updatehessianmatrix',['updateHessianMatrix',['../classOsqpEigen_1_1Solver.html#a3380601ced1a2db8b226f7a32e5dd6b3',1,'OsqpEigen::Solver']]],
+  ['updatelinearconstraintsmatrix',['updateLinearConstraintsMatrix',['../classOsqpEigen_1_1Solver.html#a4eb3a0b2bec16b00dfc8334d4ff9ab8d',1,'OsqpEigen::Solver']]],
+  ['updatelowerbound',['updateLowerBound',['../classOsqpEigen_1_1Solver.html#a48764153cec5e618021ab79586cc71b6',1,'OsqpEigen::Solver']]],
+  ['updateupperbound',['updateUpperBound',['../classOsqpEigen_1_1Solver.html#a69f1db45e10dae9c186cc0561532f018',1,'OsqpEigen::Solver']]]
+];
